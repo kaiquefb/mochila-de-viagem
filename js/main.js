ee8999cf0d52afefeef18420f3be56cf2572915d
Kaiquefb <kaique_itj@hotmail.com>
2022-11-22 11:55:59 -0300

    criando as validações

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
 
 form.addEventListener("submit", (evento) => {    
     evento.preventDefault()
 
-    console.log(evento)
-    console.log(evento.target.elements['nome'].value)
-    console.log(evento.target.elements['quantidade'].value)
-
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
 })
+
+function criaElemento(nome, quantidade) {
+
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroDoItem = document.createElement('strong')
+    numeroDoItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroDoItem)
+    novoItem.innerHTML += nome
+
+    lista.appendChild(novoItem)
+
+}
